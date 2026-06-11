b9e937e5cbf7681d6e7f4630403097497f1132a2
Turlapati Lokesh <lokeshrockz123456@gmail.com>
2026-06-11 21:01:39 +0530

    Initial backend setup - server, db config, User model

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+// Test route
+app.get('/', (req, res) => {
+  res.send('House Rent API is running...');
+});
+
+// DB Connection + Server Start
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
