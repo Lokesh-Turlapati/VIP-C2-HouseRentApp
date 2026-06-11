5b39179306c8542c7a38908c86365a1e51cb0176
Turlapati Lokesh <lokeshrockz123456@gmail.com>
2026-06-11 21:54:41 +0530

    Refactor - remove comments for clarity

--- server/server.js
+++ server/server.js
@@ -8,17 +8,17 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
 connectDB();
-// Test route
+
 app.get('/', (req, res) => {
   res.send('House Rent API is running...');
 });
 
-// DB Connection + Server Start
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
